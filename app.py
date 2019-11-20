d4a834fa81a63c114983c2ceafe2c548375da01f
Brad Donakowski <braddonako@gmail.com>
2019-11-20 11:29:21 -0700

    All routes are working

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, g
+from flask_cors import CORS
+
+import models
+
+from resources.posts import post
+
+DEBUG = True
+PORT = 8000
+
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+@app.route('/')
+def index():
+    return 'hi'
+
+CORS(post, origins=['http://localhost:3000'], supports_credentials=True)
+app.register_blueprint(post, url_prefix='/api/v1/posts/')
+
+if __name__ == '__main__':
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
