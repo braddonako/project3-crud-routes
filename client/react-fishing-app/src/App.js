5b7bd2ea34b8f846e8bfb5062171149716bb7515
georgechapman1 <georgebyron91@gmail.com>
2019-11-21 12:24:48 -0700

    registration and crud routes working

--- client/react-fishing-app/src/App.js
+++ client/react-fishing-app/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Register from './Register';
+import Login from './Login';
 import './App.css';
+import HeaderComponent from './HeaderComponent';
 import PostContainer from './PostContainer';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    <PostContainer />
-    </div>
-  );
+const My404 = () => {
+ return (
+   <div>
+     <h3>You are lost</h3>
+   </div>
+ )
 }
-
+function App() {
+ return (
+   <main>
+     <HeaderComponent />
+     <Switch>
+       <Route exact path="/" component={ Register } />
+       <Route exact path="/login" component={ Login } />
+       <Route exact path="/posts" component={ PostContainer } />
+       <Route component={My404} />
+     </Switch>
+   </main>
+ );
+}
 export default App;
---
+++ client/react-fishing-app/src/HeaderComponent/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Header, List } from 'semantic-ui-react';
+const HeaderComponent = () => {
+  return (
+    <Header>
+      <List>
+        <List.Item>
+          <Link to="/">Register</Link>
+        </List.Item>
+        <List.Item>
+          <Link to="/login">Login</Link>
+        </List.Item>
+        <List.Item>
+          <Link to="/posts">River Journal</Link>
+        </List.Item>
+      </List>
+    </Header>
+  )
+}
+export default HeaderComponent;
